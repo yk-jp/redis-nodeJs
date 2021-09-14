d4fc0c44c83f207529028818a65d5b1c7a93215b
yusuke <ID+username@users.noreply.github.com>
2021-09-14 10:03:16 +0900

    get post request with redis

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const util = require('util');
 const app = express();
 
 app.use(express.json());
@@ -8,10 +9,24 @@ const redis = require("redis");
 const redisDefaultUrl = "redis://127.0.0.1:6379";
 const client = redis.createClient(redisDefaultUrl);
 
+client.set = util.promisify(client.set);
+client.get = util.promisify(client.get);
 
-client.on("error", function(error) {
+client.on("error", (error) => {
     console.error(error);
 });
+
+app.post('/', async(req, res) => {
+    const { key, value } = req.body;
+    const response = await client.set(key, value);
+    res.json(response);
+})
+
+app.get('/', async(req, res) => {
+    const { key } = req.body;
+    const value = await client.get(key);
+    res.json(value);
+})
 
 app.listen('3000', () => {
     console.log('server is listening on port 3000');
